d2c7920aa1672db30ff181bb8ab66be3d5658565
ANAND DEV <ananddev4423169@gmail.com>
2026-07-28 14:36:18 +0530

    Completed Week 7 Spring REST hands-on

--- week 7/spring-learn/src/main/java/com/cognizant/springlearn/Country.java
+++ week 7/spring-learn/src/main/java/com/cognizant/springlearn/Country.java
@@ -1,37 +1,31 @@
 package com.cognizant.springlearn;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Country {
-
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(Country.class);
 
     private String code;
     private String name;
 
     public Country() {
-        LOGGER.debug("Inside Country Constructor.");
+    }
+
+    public Country(String code, String name) {
+        this.code = code;
+        this.name = name;
     }
 
     public String getCode() {
-        LOGGER.debug("Inside getCode()");
         return code;
     }
 
     public void setCode(String code) {
-        LOGGER.debug("Inside setCode()");
         this.code = code;
     }
 
     public String getName() {
-        LOGGER.debug("Inside getName()");
         return name;
     }
 
     public void setName(String name) {
-        LOGGER.debug("Inside setName()");
         this.name = name;
     }
 
